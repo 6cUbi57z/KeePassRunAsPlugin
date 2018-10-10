ea151ec7dacf8c863a797402c6647bf7c894ab67
Dale Edwards <gitkraken@g-de.co.uk>
2018-10-10 22:05:55 +0100

    Fixed run as options tab colour.

--- src/RunAsPlugin/UI/RunAsOptions.Designer.cs
+++ src/RunAsPlugin/UI/RunAsOptions.Designer.cs
@@ -123,6 +123,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.Controls.Add(this.runAsGroupBox);
             this.Name = "RunAsOptions";
             this.Size = new System.Drawing.Size(424, 116);
