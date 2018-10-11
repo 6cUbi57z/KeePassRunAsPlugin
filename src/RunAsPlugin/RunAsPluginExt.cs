cd4c0afd824e9ad4096137d43ec37fa36cc796cd
Dale Edwards <gitkraken@g-de.co.uk>
2018-10-11 22:48:29 +0100

    Cleanup and fixed issues when a new database is opened.

--- src/RunAsPlugin/RunAsPluginExt.cs
+++ src/RunAsPlugin/RunAsPluginExt.cs
@@ -1,23 +1,34 @@
-﻿using KeePass.Plugins;
+﻿using System.Windows.Forms;
+using KeePass.Plugins;
 using RunAsPlugin.UI;
 
 namespace RunAsPlugin
 {
+    /// <summary>
+    /// Plugin entry class for RunAs KeePass plugin.
+    /// </summary>
     public sealed class RunAsPluginExt : Plugin
     {
-        private IPluginHost m_host = null;
-
         private WindowMonitor windowMonitor;
 
+        /// <summary>
+        /// Initialises the plugin.
+        /// </summary>
+        /// <param name="host">The plugin host.</param>
+        /// <returns>True if the plugin started successfully.</returns>
         public override bool Initialize(IPluginHost host)
         {
-            m_host = host;
+            // Start the window monitor so that we can modify new password entry windows.
+            this.windowMonitor = new WindowMonitor(host);
 
-            this.windowMonitor = new WindowMonitor(host.Database);
+            // Create the run as context menu item.
+            ContextMenuStrip contextMenu = host.MainWindow.EntryContextMenu;
+            contextMenu.Items.Add(new ToolStripSeparator());
 
             RunAsMenuItem runAsMenuItem = new RunAsMenuItem(host.MainWindow);
             host.MainWindow.EntryContextMenu.Items.Add(runAsMenuItem);
 
+            // Return true for a successful startup.
             return true;
         }
     }
--- src/RunAsPlugin/UI/WindowMonitor.cs
+++ src/RunAsPlugin/UI/WindowMonitor.cs
@@ -1,21 +1,35 @@
 ﻿using KeePass.Forms;
+using KeePass.Plugins;
 using KeePass.UI;
-using KeePassLib;
 using RunAsPlugin.SafeManagement;
 
 namespace RunAsPlugin.UI
 {
+    /// <summary>
+    /// Monitors for password entry windows created by KeePass to modify them with the run as settings.
+    /// </summary>
     internal class WindowMonitor
     {
-        private readonly PwDatabase database;
+        /// <summary>
+        /// The plugin host.
+        /// </summary>
+        /// <remarks>
+        /// We have to store the host rather than the database in case the user opens an additional database.
+        /// </remarks>
+        private readonly IPluginHost host;
 
-        internal WindowMonitor(PwDatabase database)
+        internal WindowMonitor(IPluginHost host)
         {
-            this.database = database;
+            this.host = host;
 
             GlobalWindowManager.WindowAdded += this.WindowCreated;
         }
 
+        /// <summary>
+        /// Event handler for when a new window is created. Checks if this is a password entry window and, if so, attaches to the "shown" event.
+        /// </summary>
+        /// <param name="sender">The object which triggered the event.</param>
+        /// <param name="e">The event arguments.</param>
         private void WindowCreated(object sender, GwmWindowEventArgs e)
         {
             if (e.Form is PwEntryForm)
@@ -25,11 +39,16 @@ namespace RunAsPlugin.UI
             }
         }
 
+        /// <summary>
+        /// Event handler for when a password entry window is shown. Adds the "Run As" tab.
+        /// </summary>
+        /// <param name="sender">The object which triggered the event.</param>
+        /// <param name="e">The event arguments.</param>
         private void PasswordEntryFormShown(object sender, System.EventArgs e)
         {
             PwEntryForm form = (PwEntryForm)sender;
 
-            PasswordEntryManager passwordEntryManager = new PasswordEntryManager(database, form);
+            PasswordEntryManager passwordEntryManager = new PasswordEntryManager(this.host.Database, form);
 
             new RunAsTab((PwEntryForm)sender, passwordEntryManager);
         }
